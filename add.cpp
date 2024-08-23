cb1ff13e3eced7d0fa1725d2fe244c1ec0b946d1
JorgeOrtz <jorge.ortiz72@upr.edu>
2024-08-23 16:02:54 +0000

    The sum of these two integers is 25

---
+++ add.cpp
@@ -0,0 +1,15 @@
+// Jorge Ortiz soto 
+//- User Name - Jorge Ortz 
+// This program adds two numbers
+
+#include <iostream>
+
+int main() {
+  int number1, number2, sum;
+  number1 = 12;
+  number2 = 13;
+  sum = number1 + number2;
+  std::cout << "The sum of these two integers is "
+            << sum << std::endl;
+  return 0;
+}
